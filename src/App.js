b8bbfdfc7c8bbbada6e23fcf5ca3031036ce56a6
JaklinBshv <jb@auxcode.com>
2023-09-09 12:40:14 +0300

    implemented real images and reset

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useRef } from 'react';
 import Mouse from './Mouse';
-import Cheese from './Cheese';
-import "./App.css";
+import './App.css';
 
 function App() {
   const [feedback, setFeedback] = useState('');
+  const [reset, setReset] = useState(false); 
+
   const mouse1Cheeses = useRef([]);
   const mouse2Cheeses = useRef([]);
 
   const handleDrop = (mouseId, cheeseId) => {
-    if (mouseId === "mouse1") {
+    if (mouseId === 'mouse1') {
       mouse1Cheeses.current.push(cheeseId);
-    } else if (mouseId === "mouse2") {
+    } else if (mouseId === 'mouse2') {
       mouse2Cheeses.current.push(cheeseId);
     }
 
@@ -27,35 +28,56 @@ function App() {
       mouse1Cheeses.current.includes('cheese3') &&
       mouse2Cheeses.current.includes('cheese3')
     ) {
-      setFeedback('Correct! Wait 24 hours to see the result.');
+      setFeedback('Correct!');
     } else {
-      setFeedback('Try again.');
+      // setFeedback('Try again.');
     }
   };
 
-  const resetGame = () => {
+  const handleReset = () => {
     setFeedback('');
+    setReset(true);
+    setTimeout(() => setReset(false), 10);
     mouse1Cheeses.current = [];
     mouse2Cheeses.current = [];
-    window.location.reload();
   };
 
   return (
     <div className="app">
-        <div className="mice-container">
-            <Mouse id="mouse1" onDrop={handleDrop} />
-            <Mouse id="mouse2" onDrop={handleDrop} />
-        </div>
-        <div className="cheeses-container">
-            <div id="cheese1" className="cheese" draggable="true" onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese1')}></div>
-            <div id="cheese2" className="cheese" draggable="true" onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese2')}></div>
-            <div id="cheese3" className="cheese" draggable="true" onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese3')}></div>
-            <div id="cheese4" className="cheese" draggable="true" onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese4')}></div>
-        </div>
-        <p className="feedback">{feedback}</p>
-        <button onClick={() => setFeedback("")}>Reset</button>
+      <div className="mice-container">
+        <Mouse id="mouse1" onDrop={handleDrop} reset={reset} />
+        <Mouse id="mouse2" onDrop={handleDrop} reset={reset} />
+      </div>
+      <div className="cheeses-container">
+        <div
+          id="cheese1"
+          className="cheese"
+          draggable="true"
+          onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese1')}
+        ></div>
+        <div
+          id="cheese2"
+          className="cheese"
+          draggable="true"
+          onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese2')}
+        ></div>
+        <div
+          id="cheese3"
+          className="cheese"
+          draggable="true"
+          onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese3')}
+        ></div>
+        <div
+          id="cheese4"
+          className="cheese"
+          draggable="true"
+          onDragStart={(e) => e.dataTransfer.setData('text/plain', 'cheese4')}
+        ></div>
+      </div>
+      <p className="feedback">{feedback}</p>
+      <button onClick={handleReset}>Reset</button>
     </div>
-);
+  );
 }
 
 export default App;
--- src/Mouse.js
+++ src/Mouse.js
@@ -1,42 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Mouse({ id, onDrop, reset }) {
-    const [cheeses, setCheeses] = useState([]);
+  const [cheeses, setCheeses] = useState([]);
 
-    const handleDropEvent = (e) => {
-        e.preventDefault();
-        const cheeseId = e.dataTransfer.getData('text/plain');
-        if (cheeses.length < 2) {
-            const updatedCheeses = [...cheeses, cheeseId];
-            setCheeses(updatedCheeses);
-            onDrop(id, cheeseId);
-        }
-    };
-
-    const handleUndo = (index) => {
-        const updatedCheeses = [...cheeses];
-        updatedCheeses.splice(index, 1);
-        setCheeses(updatedCheeses);
-    };
-
+  useEffect(() => {
     if (reset) {
-        setCheeses([]);
+      setCheeses([]);
     }
+  }, [reset]);
 
-    return (
-        <div 
-            id={id} 
-            className="mouse" 
-            onDrop={handleDropEvent} 
-            onDragOver={(e) => e.preventDefault()}
-        >
-            <div className="small-cheeses-container">
-                {cheeses.map((cheeseId, idx) => (
-                    <div key={idx} className="small-cheese" onClick={() => handleUndo(idx)}></div>
-                ))}
-            </div>
-        </div>
-    );
+  const handleDropEvent = (e) => {
+    e.preventDefault();
+    const cheeseId = e.dataTransfer.getData('text/plain');
+    if (cheeses.length < 2) {
+      const updatedCheeses = [...cheeses, cheeseId];
+      setCheeses(updatedCheeses);
+      onDrop(id, cheeseId);
+    }
+  };
+
+  const handleUndo = (index) => {
+    const updatedCheeses = [...cheeses];
+    updatedCheeses.splice(index, 1);
+    setCheeses(updatedCheeses);
+  };
+
+  return (
+    <div
+      id={id}
+      className="mouse"
+      onDrop={handleDropEvent}
+      onDragOver={(e) => e.preventDefault()}
+    >
+      <div className="small-cheeses-container">
+        {cheeses.map((cheeseId, idx) => (
+          <div key={idx} className="small-cheese" onClick={() => handleUndo(idx)}></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Mouse;
